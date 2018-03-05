8980b31e4a22ea8bab49db47b4ec61219448a939
Gustavo Santos Ferreira <gustavosantosfe@gmail.com>
2018-03-05 16:45:19 -0300

    test all possible basic logic operations

---
+++ test/logic_gates_test.exs
@@ -0,0 +1,23 @@
+defmodule LogicGatesTest do
+  use ExUnit.Case
+  doctest LogicGates
+
+  test "and_gate makes 'and' operation" do
+    assert LogicGates.and_gate(true, true) == true
+    assert LogicGates.and_gate(true, false) == false
+    assert LogicGates.and_gate(false, true) == false
+    assert LogicGates.and_gate(false, false) == false
+  end
+
+  test "or_gate makes 'or' operation" do
+    assert LogicGates.or_gate(true, true) == true
+    assert LogicGates.or_gate(true, false) == true
+    assert LogicGates.or_gate(false, true) == true
+    assert LogicGates.or_gate(false, false) == false
+  end
+
+  test "not_gate makes 'not' operation" do
+    assert LogicGates.not_gate(true) == false
+    assert LogicGates.not_gate(false) == true
+  end
+end
